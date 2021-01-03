a3e8c81aeb4b0e89bd96e45b48fe31ef25617e9c
Sergi Obiols Olcina <sergiobi23@MBP-de-Sergi.home>
2021-01-03 11:32:33 +0100

    api init

---
+++ src/main/java/com/example/tfg/TfGapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfGapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfGapiApplication.class, args);
+    }
+
+}
