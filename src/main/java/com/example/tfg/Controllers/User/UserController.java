46285e260cceb2a11cb90f0073c72982fa2023dc
Sergi <sergiobiolsolcina@gmail.com>
2021-03-13 09:57:21 +0100

    check user by email

--- src/main/java/com/example/tfg/Controllers/User/UserController.java
+++ src/main/java/com/example/tfg/Controllers/User/UserController.java
@@ -14,7 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -35,9 +37,23 @@ public class UserController {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         Role userRole = roleRepository.findByRole(Constants.USER_ROLE);
         user.setRoles(new HashSet<>(Collections.singletonList(userRole)));
+        user.setUserRole(Constants.USER_ROLE);
 
         userRepository.insert(user);
 
         return new ResponseEntity(user, HttpStatus.valueOf(200));
+    }
+
+    @PostMapping("/existing/{email}")
+    public ResponseEntity checkUserAlreadyExists(@PathVariable String email) {
+        Map<Object, Object> model = new HashMap<>();
+        User existingUser = userRepository.findUserByEmail(email);
+
+        if (existingUser != null) {
+            model.put("info", Constants.USER_EXISTS);
+        } else {
+            model.put("info", Constants.USER_NOT_EXIST);
+        }
+        return new ResponseEntity(model, HttpStatus.valueOf(200));
     }
 }
--- src/main/java/com/example/tfg/Helpers/Constants.java
+++ src/main/java/com/example/tfg/Helpers/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 
     public static final String USER_ROLE = "userRole", ARTIST_ROLE = "artistRole";
     public static final int EARTH_RADIUS = 6371; // Radius of the earth
+    public static final String USER_EXISTS = "USER ALREADY EXISTS", USER_NOT_EXIST = "USER NOT EXIST";
 
 }
